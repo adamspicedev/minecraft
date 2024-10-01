a549da3864c7d0777a60903a5de7cb377c8891cb
Adam Spice <adam@spicey.dev>
2024-10-01 14:30:17 +1300

    added procedural world generation

--- src/main.js
+++ src/main.js
@@ -1,14 +1,10 @@
 import * as THREE from "three";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-import Stats from "three/examples/jsm/libs/stats.module";
-
+import Stats from "three/examples/jsm/libs/stats.module.js";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 import { World } from "./world";
-import { createUI } from "./ui";
-// Setup stats
-const stats = new Stats();
-document.body.appendChild(stats.dom);
+import { setupUI } from "./ui";
 
-// Setup the renderer
+// Renderer setup
 const renderer = new THREE.WebGLRenderer();
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -18,52 +14,56 @@ document.body.appendChild(renderer.domElement);
 // Camera setup
 const camera = new THREE.PerspectiveCamera(
   75,
-  window.innerWidth / window.innerHeight
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
 );
-camera.position.set(-32, 16, -32);
+camera.position.set(45, 45, 45);
+camera.lookAt(0, 0, 0);
 
-// Controls setup
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.target.set(16, 0, 16);
 controls.update();
 
 // Scene setup
 const scene = new THREE.Scene();
-
-// Lights setup
-function setupLights() {
-  const ambientLight = new THREE.AmbientLight();
-  ambientLight.intensity = 0.1;
-  scene.add(ambientLight);
-
-  const directionalLight1 = new THREE.DirectionalLight();
-  directionalLight1.position.set(1, 1, 1);
-  scene.add(directionalLight1);
-
-  const directionalLight2 = new THREE.DirectionalLight();
-  directionalLight2.position.set(-1, 1, 0.5);
-  scene.add(directionalLight2);
-}
-
-// Setup world
 const world = new World();
 world.generate();
 scene.add(world);
 
-// Render loop
-function animate() {
-  requestAnimationFrame(animate);
-  controls.update();
-  renderer.render(scene, camera);
-  stats.update();
+function setupLighting() {
+  const light1 = new THREE.DirectionalLight();
+  light1.position.set(1, 1, 1);
+  scene.add(light1);
+
+  const light2 = new THREE.DirectionalLight();
+  light2.position.set(-1, 1, -0.5);
+  scene.add(light2);
+
+  const ambient = new THREE.AmbientLight();
+  ambient.intensity = 0.1;
+  scene.add(ambient);
 }
 
+// Events
 window.addEventListener("resize", () => {
+  // Resize camera aspect ratio and renderer size to the new window size
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
   renderer.setSize(window.innerWidth, window.innerHeight);
 });
 
-setupLights();
-createUI(world);
+// UI Setup
+const stats = new Stats();
+document.body.appendChild(stats.dom);
+
+// Render loop
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+  stats.update();
+}
+
+setupUI(world);
+setupLighting();
 animate();
